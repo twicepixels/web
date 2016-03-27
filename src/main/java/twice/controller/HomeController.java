f2637c8ee78807d5ce733b9153360312ebccc888
dfonseca <tuma05@gmail.com>
2016-03-26 18:18:01 -0600

    Utilizar thymeleaf para los templates. Crear WebMvcConfig para poder usar la carpeta webapp Incluir la configuración inicial de la seguridad en la app

--- src/main/java/twice/controller/HomeController.java
+++ src/main/java/twice/controller/HomeController.java
@@ -1,12 +1,24 @@
 package twice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Locale;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    MessageSource msgSource;
+
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        String msg = msgSource.getMessage("view.index.hello",
+                null, "Hello world!", Locale.US);
+        model.addAttribute("message", msg);
         return "index";
     }
 }
