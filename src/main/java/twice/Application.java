db1989a8e1966c09267e435dc320b5bf66527fd8
dfonseca <tuma05@gmail.com>
2016-03-26 18:17:42 -0600

    Utilizar thymeleaf para los templates. Crear WebMvcConfig para poder usar la carpeta webapp Incluir la configuración inicial de la seguridad en la app

--- src/main/java/twice/Application.java
+++ src/main/java/twice/Application.java
@@ -1,13 +1,9 @@
 package twice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
 @SpringBootApplication
-@EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
