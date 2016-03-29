977fb944dae87a161e879628be7c7723cd81cd09
dfonseca <tuma05@gmail.com>
2016-03-29 17:16:06 -0600

    Corregir vista, daba error en algunos casos

--- src/main/java/twice/config/Interceptor/TestInterceptor.java
+++ src/main/java/twice/config/Interceptor/TestInterceptor.java
@@ -7,10 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class TestInterceptor implements HandlerInterceptor {
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
                              Object handler) throws Exception {
-        System.out.println("preHandle");
         return true;
     }
 
